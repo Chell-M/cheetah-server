178a6f05f09131f784e714372e0c930ed9176a7b
Michael Mele <michaelmele@Michaels-MacBook-Air.local>
2024-10-18 14:00:24 -0700

    sockets connect but dont display live updates

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -8,10 +8,31 @@ export const socketHandler = (io) => {
       socket.join(gameId);
 
       const clients = io.sockets.adapter.rooms.get(gameId);
+
       if (clients && clients.size === 2) {
-        console.log(`Both users are in game: ${gameId}. Starting game...`);
+        console.log(`Both users are in game: ${gameId}. starting game...`);
         io.in(gameId).emit("startGame", { gameId });
+
+        // Listen for cursor updates and broadcast to the other user
+        /* socket.on("cursorUpdate", ({ gameId, userId, cursorIndex }) => {
+          socket
+            .to(gameId)
+            .emit("updateOpponentsCursor", { userId, cursorIndex });
+        });
+*/
       }
+
+      // Emit participantJoined event to all clients in the room
+      io.in(gameId).emit("participantJoined", {
+        gameId,
+        participants: Array.from(clients).map((clientId) => {
+          const clientSocket = io.sockets.sockets.get(clientId);
+          return {
+            userId: clientSocket.userId,
+            username: clientSocket.username,
+          };
+        }),
+      });
     });
 
     // Handle user disconnect
