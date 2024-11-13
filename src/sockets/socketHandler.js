8d11f92f55665825a295dab159ce2f987ef58553
Michael Mele <michaelmele@Michaels-MacBook-Air.local>
2024-11-13 16:19:37 -0700

    MatchMaking socket and gameUpdate working

--- src/sockets/socketHandler.js
+++ src/sockets/socketHandler.js
@@ -1,60 +1,59 @@
-import {
-  getOpenGames,
-  createNewGame,
-  addUserToGameService,
-} from "../services/gameService.js";
+import { get } from "mongoose";
+import { getOpenGames, createNewGame, addUserToGameService, getUserGame } from "../services/gameService.js";
+
+// `fetchOpenGames` fetch games with status "open"
+//  `createNewGame` create new game object with user inside
+//   `addUserToGameService` if a game is open, push user in participants
 
 const socketHandler = (io) => {
   console.log("socketHandler initialized");
+
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
     });
+
+    socket.on("joinGame", async ({ userId }) => {
+      try {
+        const openGames = await getOpenGames();
+        console.log("Open games:", openGames);
+        let game;
+        if (openGames.length === 0) {
+          game = await createNewGame(userId);
+          console.log(`Created new game with ID: ${game.gameId}`);
+        } else {
+          const gameId = openGames[0].gameId;
+          game = await addUserToGameService(gameId, userId);
+          console.log(`User ${userId} joined game ${game.gameId}`);
+        }
+        socket.join(game.gameId);
+
+        const room = io.sockets.adapter.rooms.get(game.gameId);
+        const numberOfClients = room ? room.size : 0;
+        console.log(`Number of clients in ${game.gameId} ${numberOfClients}`);
+
+        if (numberOfClients) {
+          // Emit the updated game state to all participants
+          io.to(game.gameId).emit("updateGameState", game);
+          console.log(game);
+        }
+      } catch (error) {
+        socket.emit("error", {
+          message: error.message || "Failed to join game",
+        });
+      }
+    });
+
+    socket.on("cursorUpdate", ({ gameId, userId, cursorIndex }) => {
+      socket.to(gameId).emit("cursorUpdate", { userId, cursorIndex });
+    });
+
+    socket.on("gameResults", ({ gameId, results }) => {
+      io.to(gameId).emit("gameResults", results);
+    });
   });
 };
 
 export default socketHandler;
-// socket.on("joinGame", async ({ userId }) => {
-//   try {
-//     // Fetch open games
-//     const openGames = await getOpenGames();
-//     console.log("Open games:", openGames);
-//     let game;
-
-//     if (openGames.length === 0) {
-//       // No open games found; create a new game
-//       game = await createNewGame(userId);
-//       console.log(`Created new game with ID: ${game.gameId}`);
-//     } else {
-//       // Join the first available open game
-//       const gameId = openGames[0].gameId;
-//       game = await addUserToGameService(gameId, userId);
-//       console.log(`User ${userId} joined game ${game.gameId}`);
-//     }
-
-//     // Join the Socket.IO room based on `gameId`
-//     socket.join(game.gameId);
-//     console.log(socket.rooms);
-//     const room = io.sockets.adapter.rooms.get(game.gameId);
-//     const numberOfClients = room ? room.size : 0;
-//     console.log(`Number of clients in ${game.gameId} ${numberOfClients}`);
-
-//     // Emit the updated game state to all participants
-//     io.to(game.gameId).emit("updateGameState", game);
-//   } catch (error) {
-//     console.error("Error handling join game:", error);
-//     socket.emit("error", {
-//       message: error.message || "Failed to join game",
-//     });
-//   }
-// });
-
-// socket.on("cursorUpdate", ({ gameId, userId, cursorIndex }) => {
-//   socket.to(gameId).emit("cursorUpdate", { userId, cursorIndex });
-// });
-
-// socket.on("gameResults", ({ gameId, results }) => {
-//   io.to(gameId).emit("gameResults", results);
-// });
