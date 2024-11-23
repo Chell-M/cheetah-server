6cc6c78e477ecd802d4b8f3aae592f6a6977019a
Michele Mele <chellymele@gmail.com>
2024-11-23 11:43:17 -0700

    setting deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
@@ -32,7 +32,7 @@ app.use("/api", profileRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
